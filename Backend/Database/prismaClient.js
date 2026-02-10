baefe3ed3fe15d5b974225fe482ff9d5c197c485
mkhalfadel <fadel.mokahal@gmail.com>
2026-02-10 21:07:13 +0200

    Fixed prisma client issue

--- Backend/Database/prismaClient.js
+++ Backend/Database/prismaClient.js
@@ -1,12 +1,6 @@
-require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
+// require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-    }
-  }
-});
+const prisma = new PrismaClient(); // uses schema.prisma datasource which reads env("DATABASE_URL")
 
 module.exports = prisma;
